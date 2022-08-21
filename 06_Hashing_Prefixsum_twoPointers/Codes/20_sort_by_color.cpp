6557dce8b0c5eeb713c44692bb0bcbeb2c6c181b
Saurabh Kumar <asksaurabh101@gmail.com>
2022-08-21 10:28:49 +0530

    New

---
+++ 06_Hashing_Prefixsum_twoPointers/Codes/20_sort_by_color.cpp
@@ -0,0 +1,11 @@
+void Solution::sortColors(vector<int> &nums) {
+
+    int n = nums.size();
+    int low = 0, mid = 0, high = nums.size() - 1;
+
+    while (mid <= high) {
+        if (nums[mid] == 0) swap(nums[low++], nums[mid++]);
+        else if (nums[mid] == 2) swap(nums[mid], nums[high--]);
+        else mid++;
+    }
+}
